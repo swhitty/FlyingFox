ee0118895184f052a96b425869a76553864610a9
Simon Whitty <simon@whileloop.com>
2022-10-01 08:38:40 +1000

    Only add if events exist

--- FlyingSocks/Sources/EventQueue+ePoll.swift
+++ FlyingSocks/Sources/EventQueue+ePoll.swift
@@ -87,7 +87,7 @@ struct ePoll: EventQueue {
                     throw SocketError.makeFailed("epoll_ctl EPOLL_CTL_MOD")
                 }
             }
-        } else {
+        } else if !events.isEmpty {
             guard epoll_ctl(file.rawValue, EPOLL_CTL_ADD, socket.rawValue, &event) != -1 else {
                 throw SocketError.makeFailed("epoll_ctl EPOLL_CTL_ADD")
             }
