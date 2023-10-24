f37ba563efd7352babe92d7a7c67449b89f28b9a
Simon Whitty <simon@whileloop.com>
2023-10-24 19:48:53 +1100

    remove dead code

--- FlyingFox/Sources/HTTPRoute.swift
+++ FlyingFox/Sources/HTTPRoute.swift
@@ -190,16 +190,3 @@ extension HTTPRoute: ExpressibleByStringLiteral {
         self.init(value)
     }
 }
-
-
-private extension Sequence {
-
-    func first(where predicate: (Element) async throws -> Bool) async rethrows -> Element? {
-        for element in self {
-            if try await predicate(element) {
-                return element
-            }
-        }
-        return nil
-    }
-}
