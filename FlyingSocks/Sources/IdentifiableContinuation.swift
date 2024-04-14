ca64e8936bf31c59382b5bbd7ef3513138d9120b
Simon Whitty <simon@whileloop.com>
2024-04-14 18:41:56 +1000

    #function

--- FlyingSocks/Sources/IdentifiableContinuation.swift
+++ FlyingSocks/Sources/IdentifiableContinuation.swift
@@ -45,13 +45,14 @@
 @_spi(Private)
 public func withIdentifiableContinuation<T>(
   isolation: isolated some Actor,
+  function: String = #function,
   body: (IdentifiableContinuation<T, Never>) -> Void,
   onCancel handler: @Sendable (IdentifiableContinuation<T, Never>.ID) -> Void
 ) async -> T {
     let id = IdentifiableContinuation<T, Never>.ID()
     let state = AllocatedLock(initialState: (isStarted: false, isCancelled: false))
     return await withTaskCancellationHandler {
-        await withCheckedContinuation {
+        await withCheckedContinuation(function: function) {
             let continuation = IdentifiableContinuation(id: id, continuation: $0)
             body(continuation)
             let sendCancel = state.withLock {
@@ -90,13 +91,14 @@ public func withIdentifiableContinuation<T>(
 @_spi(Private)
 public func withIdentifiableThrowingContinuation<T>(
   isolation: isolated some Actor,
+  function: String = #function,
   body: (IdentifiableContinuation<T, any Error>) -> Void,
   onCancel handler: @Sendable (IdentifiableContinuation<T, any Error>.ID) -> Void
 ) async throws -> T {
     let id = IdentifiableContinuation<T, any Error>.ID()
     let state = AllocatedLock(initialState: (isStarted: false, isCancelled: false))
     return try await withTaskCancellationHandler {
-        try await withCheckedThrowingContinuation {
+        try await withCheckedThrowingContinuation(function: function) {
             let continuation = IdentifiableContinuation(id: id, continuation: $0)
             body(continuation)
             let sendCancel = state.withLock {
