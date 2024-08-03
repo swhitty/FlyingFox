197234c6c5357027aede936b82362ef24fd57a07
Simon Whitty <simon@whileloop.com>
2024-08-03 16:37:24 +1000

    typo

--- FlyingSocks/Tests/AsyncBufferedDataSequenceTests.swift
+++ FlyingSocks/Tests/AsyncBufferedDataSequenceTests.swift
@@ -35,7 +35,7 @@ import XCTest
 
 final class AsyncBufferedDataSequenceTests: XCTestCase {
 
-    func testSeqeunce() async {
+    func testSequence() async {
         let buffer = AsyncBufferedCollection(bytes: [
             0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
             0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF
