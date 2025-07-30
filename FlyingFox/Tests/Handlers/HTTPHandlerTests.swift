957494a7271f8036f22ab22dbbe995310a41825b
Nikolay Spassov <nikolay@apperi.net>
2025-07-30 09:51:47 +0300

    Update tests

--- FlyingFox/Tests/Handlers/HTTPHandlerTests.swift
+++ FlyingFox/Tests/Handlers/HTTPHandlerTests.swift
@@ -177,6 +177,9 @@ struct HTTPHandlerTests {
             FileHTTPHandler.makeContentType(for: "fish.jp2") == "image/jp2"
         )
         #expect(
+            FileHTTPHandler.makeContentType(for: "fish.properties") == "text/plain"
+        )
+        #expect(
             FileHTTPHandler.makeContentType(for: "fish.somefile") == "application/octet-stream"
         )
     }
